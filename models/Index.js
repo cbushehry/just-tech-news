1529ed61a6af5652276ffa1bb556497325699fef
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-28 16:38:46 -0700

    finished bcrypt section. pw now hashed

--- models/Index.js
+++ models/Index.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcrypt');
 const User = require('./User');
 
 module.exports = { User };
--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields/columns for User model
 User.init(
@@ -36,10 +42,14 @@ User.init(
   {
     hooks: {
       // set up beforeCreate lifecycle "hook" functionality
-      beforeCreate(userData) {
-        return bcrypt.hash(userData.password, 10).then(newUserData => {
-          return newUserData
-        });
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       }
     },
     sequelize,
@@ -49,6 +59,5 @@ User.init(
     modelName: 'user'
   }
 );
-
 
 module.exports = User;
